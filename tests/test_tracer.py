bde033eb6940165f71a1affb5cb76b152c1273ed
Doug Hellmann <doug.hellmann@gmail.com>
2013-05-26 12:12:51 -0400

    clean up tracer test module
    Signed-off-by: Doug Hellmann <doug.hellmann@gmail.com>

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -35,36 +35,3 @@ class TracerTest(testtools.TestCase):
     def test_ignore_smiley(self):
         t = tracer.Tracer(None)
         self.assertTrue(t._should_ignore_file(tracer.__file__))
-
-    # @mock.patch('zmq.Context')
-    # def test_socket_setup(self, context_factory):
-    #     listener.Listener('endpoint')
-    #     context = context_factory.return_value
-    #     context.socket.assert_called_with(zmq.PULL)
-    #     s = context.socket.return_value
-    #     s.connect.assert_called_with('endpoint')
-
-    # @mock.patch('zmq.Context')
-    # @mock.patch('smiley.listener.Listener.poll_once')
-    # def test_poll_breaks(self, meth, context):
-    #     meth.side_effect = KeyboardInterrupt
-    #     cb = mock.MagicMock()
-    #     cb.side_effect = AssertionError('should not be called')
-    #     l = listener.Listener('endpoint')
-    #     l.poll_forever(cb)
-
-    # @mock.patch('zmq.Context.socket')
-    # @mock.patch('zmq.Poller.poll')
-    # def test_unpack_message(self, poll, socket):
-    #     poll.return_value = [(socket, zmq.POLLIN)]
-    #     msg = {
-    #         'key': 'value',
-    #         'key2': ['v1', 1],
-    #     }
-    #     socket.return_value.recv_multipart.return_value = (
-    #         'message type name',
-    #         json.dumps(msg),
-    #     )
-    #     l = listener.Listener('endpoint')
-    #     val = next(l.poll_once())
-    #     self.assertEqual(val, ['message type name', msg])
