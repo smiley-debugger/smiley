1e1273c502d078a6349f296dc699205d70b64058
Doug Hellmann <doug.hellmann@gmail.com>
2013-06-06 20:04:28 -0400

    add -e option to exit with an error
    Signed-off-by: Doug Hellmann <doug.hellmann@gmail.com>

--- test_app/test.py
+++ test_app/test.py
@@ -1,5 +1,9 @@
+import sys
+
 import test_funcs
 
 if __name__ == '__main__':
     print 'Running', __file__
     test_funcs.a()
+    if '-e' in sys.argv:
+        raise RuntimeError('test exception')
