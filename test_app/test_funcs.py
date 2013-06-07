2423f974c84c6656092ca83b1ba4ad1a874d769e
Doug Hellmann <doug.hellmann@gmail.com>
2013-06-06 20:07:18 -0400

    Add testing for exceptions
    Have the test program throw and catch an exception
    so the tracer code can be exercised in that case.
    
    Signed-off-by: Doug Hellmann <doug.hellmann@gmail.com>

--- test_app/test_funcs.py
+++ test_app/test_funcs.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+
 import sys
+
 
 def gen(m):
     for i in xrange(m):
@@ -6,19 +9,27 @@ def gen(m):
 
 
 def c(input):
-    print 'input =', input
+    print('input =', input)
     data = list(gen(input))
-    print 'Leaving c()', data
+    print('Leaving c()', data)
 
 
 def b(arg):
     val = arg * 5
     c(val)
-    print 'Leaving b()'
+    print('Leaving b()')
     return val
 
 
+def produce_error():
+    try:
+        b('invalid')
+    except Exception as err:
+        print('Error:', err)
+
+
 def a():
-    print 'args:', sys.argv
+    print('args:', sys.argv)
     b(2)
-    print 'Leaving a()'
+    produce_error()
+    print('Leaving a()')
