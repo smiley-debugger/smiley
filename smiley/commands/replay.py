f8a08ae517ddfdea47e63d44ec3506c4151c263f
Doug Hellmann <doug.hellmann@gmail.com>
2013-06-23 11:24:48 -0400

    remove unused code from replay command
    Signed-off-by: Doug Hellmann <doug.hellmann@gmail.com>

--- smiley/commands/replay.py
+++ smiley/commands/replay.py
@@ -30,48 +30,6 @@ class Replay(command.Command):
         )
         return parser
 
-    def _process_message(self, msg):
-        msg_type, msg_payload = msg
-        self.log.debug('MESSAGE: %s %r', msg_type, msg_payload)
-
-        if msg_type == 'start_run':
-            command_line = ' '.join(msg_payload.get('command_line', []))
-            self.log.info(
-                'Starting new run: %s',
-                command_line,
-            )
-            self._cwd = msg_payload.get('cwd', '')
-            if self._cwd:
-                self._cwd = self._cwd.rstrip(os.sep) + os.sep
-            self.db.start_run(
-                run_id=msg_payload['run_id'],
-                cwd=self._cwd,
-                description=command_line,
-                start_time=msg_payload.get('timestamp'),
-            )
-
-        elif msg_type == 'end_run':
-            self.log.info('Finished run')
-            self.db.end_run(
-                run_id=msg_payload['run_id'],
-                end_time=msg_payload.get('timestamp'),
-                message=msg_payload.get('message'),
-                traceback=msg_payload.get('traceback'),
-            )
-
-        else:
-            self.db.trace(
-                run_id=msg_payload['run_id'],
-                call_id=msg_payload['call_id'],
-                event=msg_type,
-                func_name=msg_payload.get('func_name'),
-                line_no=msg_payload.get('line_no'),
-                filename=msg_payload.get('filename'),
-                trace_arg=msg_payload.get('arg'),
-                local_vars=msg_payload.get('local_vars'),
-                timestamp=msg_payload.get('timestamp'),
-            )
-
     def take_action(self, parsed_args):
         self.db = db.DB(parsed_args.database)
         cache = db_linecache.DBLineCache(self.db, parsed_args.run_id)
