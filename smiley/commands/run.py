fff732c3eec276ae89765c73c48d6e403ccd9793
Doug Hellmann <doug.hellmann@gmail.com>
2013-07-14 15:46:54 -0400

    Add runner mode to write to local database
    Add mode options to the run command to specify whether
    the data should be sent over the network or written to
    a local database file.
    
    Fixes #11
    
    Change-Id: I992d61cfec476937187c68f28d8889b5041ed19a
    Signed-off-by: Doug Hellmann <doug.hellmann@gmail.com>

--- smiley/commands/run.py
+++ smiley/commands/run.py
@@ -4,7 +4,9 @@ import sys
 
 from cliff import command
 
-from smiley import publisher, tracer
+from smiley import db
+from smiley import publisher
+from smiley import tracer
 
 
 class Run(command.Command):
@@ -15,15 +17,36 @@ class Run(command.Command):
 
     def get_parser(self, prog_name):
         parser = super(Run, self).get_parser(prog_name)
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument(
+            '--local',
+            action='store_const',
+            dest='mode',
+            const='local',
+            help='write data to a local database',
+        )
+        group.add_argument(
+            '--remote',
+            action='store_const',
+            dest='mode',
+            const='remote',
+            default='remote',
+            help='send data to the remote monitor (default)',
+        )
         parser.add_argument(
-            'command',
-            nargs='+',
-            help='the command to spy on',
+            '--database',
+            default='smiley.db',
+            help='filename for the database (%(default)s)',
         )
         parser.add_argument(
             '--socket',
             default='tcp://127.0.0.1:5556',
             help='URL for the socket where the listener will be (%(default)s)',
+        )
+        parser.add_argument(
+            'command',
+            nargs='+',
+            help='the command to spy on',
         )
         return parser
 
@@ -37,7 +60,10 @@ class Run(command.Command):
         sys.argv = parsed_args.command
 
         # Run the app
-        p = publisher.Publisher(parsed_args.socket)
+        if parsed_args.mode == 'remote':
+            p = publisher.Publisher(parsed_args.socket)
+        else:
+            p = db.DB(parsed_args.database)
         t = tracer.Tracer(p)
         t.run(parsed_args.command)
         return
