fdf2af1f2a07852baf905c45411b96603fc78e87
Doug Hellmann <doug.hellmann@gmail.com>
2013-07-28 10:12:58 -0400

    Add the call graph view
    Turn the profile data into a call graph using gprof2dot and
    dot to render the png.
    
    Change-Id: I635ef08582185db9414913bdd840efcd4150734f

--- smiley/web/controllers/stats.py
+++ smiley/web/controllers/stats.py
@@ -46,3 +46,28 @@ class StatsController(object):
             'getfileid': functools.partial(request.db.get_file_signature,
                                            run_id=run_id),
         }
+
+    @expose(generic=True, template='graph.html')
+    @nav.active_section('runs', 'graph')
+    def graph(self, run_id):
+        run = request.db.get_run(run_id)
+        return {
+            'run_id': run_id,
+            'run': run,
+        }
+
+    @expose(content_type='image/png')
+    @nav.active_section('runs', 'graph')
+    def graph_data(self, run_id):
+        run = request.db.get_run(run_id)
+        try:
+            with tempfile.NamedTemporaryFile(mode='w') as f:
+                run.stats.dump_stats(f.name)
+                image_data = subprocess.check_output(
+                    'gprof2dot -f pstats %s | dot -Tpng' % f.name,
+                    shell=True,
+                )
+        except:
+            LOG.exception('could not generate image')
+            raise
+        return image_data
