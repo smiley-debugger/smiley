8b20d11a5908d323fc3299b80429d1c752cf09b5
Doug Hellmann <doug@doughellmann.com>
2014-08-01 13:35:00 -0400

    run multiple threads in test app
    Change-Id: I1970012ce3f392f7979481560ef6a7f8348e070d

--- test_app/test.py
+++ test_app/test.py
@@ -6,10 +6,15 @@ import test_funcs
 if __name__ == '__main__':
     print 'Running', __file__
     if '-t' in sys.argv:
-        print 'Starting a thread'
-        t = threading.Thread(target=test_funcs.a)
-        t.start()
-        t.join()
+        threads = []
+        for i in range(3):
+            t = threading.Thread(target=test_funcs.a)
+            print 'Starting thread', t.name
+            t.start()
+            threads.append(t)
+        for t in threads:
+            print 'Waiting for', t.name
+            t.join()
     else:
         test_funcs.a()
     if '-e' in sys.argv:
