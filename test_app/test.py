19d18162ae81b9673f8f65e33b0ddc89b8582d60
Doug Hellmann <doug@doughellmann.com>
2014-08-01 12:56:37 -0400

    make the test app use a thread
    Change-Id: I40d2654bd30b24c66334d77162133dd28ab6c009

--- test_app/test.py
+++ test_app/test.py
@@ -1,9 +1,16 @@
 import sys
+import threading
 
 import test_funcs
 
 if __name__ == '__main__':
     print 'Running', __file__
-    test_funcs.a()
+    if '-t' in sys.argv:
+        print 'Starting a thread'
+        t = threading.Thread(target=test_funcs.a)
+        t.start()
+        t.join()
+    else:
+        test_funcs.a()
     if '-e' in sys.argv:
         raise RuntimeError('test exception')
